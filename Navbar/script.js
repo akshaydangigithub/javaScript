f27c857a302495f10dc960b73b3c616b8807c1d1
Hariom dangi <hdangi224@gmail.com>
2024-05-16 19:48:53 +0530

    temprature convertor

--- Navbar/script.js
+++ Navbar/script.js
@@ -3,22 +3,40 @@ var line1 = document.querySelector(".line1");
 var line2 = document.querySelector(".line2");
 var sidebar = document.querySelector(".sidebar");
 
+// var flag = 0;
+
+// menu.addEventListener("click", function () {
+//   if (flag == 0) {
+//     line1.classList.add("active");
+//     line2.classList.add("active");
+//     // sidebar.style.transform = "translateX(0)";
+//     // sidebar.style.borderTopLeftRadius = "0"
+//     // sidebar.style.borderBottomLeftRadius = "0"
+//     sidebar.style.display = "block";
+//     flag = 1;
+//   } else {
+//     line1.classList.remove("active");
+//     line2.classList.remove("active");
+//     // sidebar.style.transform = "translateX(100%)";
+//     // sidebar.style.borderTopLeftRadius = "50%"
+//     // sidebar.style.borderBottomLeftRadius = "50%"
+//     sidebar.style.display = "none";
+//     flag = 0;
+//   }
+// });
+
 var flag = 0;
 
 menu.addEventListener("click", function () {
   if (flag == 0) {
     line1.classList.add("active");
     line2.classList.add("active");
-    sidebar.style.transform = "translateX(0)";
-    sidebar.style.borderTopLeftRadius = "0"
-    sidebar.style.borderBottomLeftRadius = "0"
+    sidebar.classList.add("active");
     flag = 1;
   } else {
     line1.classList.remove("active");
     line2.classList.remove("active");
-    sidebar.style.transform = "translateX(100%)";
-    sidebar.style.borderTopLeftRadius = "50%"
-    sidebar.style.borderBottomLeftRadius = "50%"
+    sidebar.classList.remove("active");
     flag = 0;
   }
 });
